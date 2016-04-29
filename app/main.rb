5c452ddb6edbbaeea2ebb25091e537d2a5a34a27
Santiago Palladino <spalladino@gmail.com>
2016-04-29 16:40:56 -0300

    Show messages per user

--- app/main.rb
+++ app/main.rb
@@ -1,5 +1,24 @@
+use ActiveRecord::ConnectionAdapters::ConnectionManagement
+
 set :views, File.expand_path('../views', __FILE__)
+set :public_folder, File.expand_path('../public', __FILE__)
 
 get '/' do
+  @users = User.all
   erb :index, layout: :layout
+end
+
+get '/users' do
+  @users = User.all
+  erb :index, layout: :layout
+end
+
+get '/users/:id' do
+  @user = User.find(params[:id])
+  @messages = @user.message_logs.order('message_logs.id DESC')
+  erb :user, layout: :layout
+end
+
+after do
+  ActiveRecord::Base.clear_active_connections!
 end
